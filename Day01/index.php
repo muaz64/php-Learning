0cfebd36b57d404eac4843e2cfac9762bd2e4c58
muaz64 <muazmuhammad21@gmail.com>
2025-03-18 00:02:34 +0600

    first commit

---
+++ Day01/index.php
@@ -0,0 +1,9 @@
+<!-- PHP Syntax Basics -->
+<?php
+echo "Hello, Everyone! Welcome to <b>PHP</b>.";
+print "Hello, World!"; 
+//Both are used to output data. echo is faster, but print returns a value (1), which can be useful
+?>
+<!-- End PHP Code Blocks:
+You can mix PHP with HTML. End the block when switching to HTML.-->
+<p>This is HTML content.</p>
