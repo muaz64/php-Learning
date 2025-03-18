e37fc99a473a5351aad03717fa9757875a889cb1
muaz64 <muazmuhammad21@gmail.com>
2025-03-18 11:43:13 +0600

    day02 php update

---
+++ Day02/index.php
@@ -0,0 +1,18 @@
+<?php
+// Variables in PHP 
+
+ $name ="Muaz";
+ $age ="25";
+ $isStudent = true ;
+
+// echo "My name is $name and i am $age years old."
+
+//Constants in PHP
+
+// define("SIte_Name", "Muaz's Website");
+// echo SIte_Name;
+
+//Checking Data Types
+$var = 10;
+echo gettype($var);  // Outputs: integer
+?>
