ec04376b1c6cff48736987ecd30ab48720da3cb9
muaz64 <muazmuhammad21@gmail.com>
2025-03-19 14:59:36 +0600

    day03 php update

--- Day03/index.php
+++ Day03/index.php
@@ -1,1 +1,11 @@
 <!-- Operators & Expressions -->
+<?php
+ $x = "5";
+ if ($x == 5) { echo "True"; } // Outputs: True
+ if ($x === 5) { echo "True"; } // Outputs nothing
+$result1 = (10 + 5) * 2;  
+echo $result1 ;// Outputs: 30
+$result2= (10 - 5) * 2; 
+echo $result2 ;// Outputs: 10
+echo $result1/$result2  // Outputs: 3
+?>
