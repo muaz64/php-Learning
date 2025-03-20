99b90001e48b5c4509c64fcb45ac9b3b1a328389
muaz64 <muazmuhammad21@gmail.com>
2025-03-20 14:19:31 +0600

    d4 php update2

--- Day04/index.php
+++ Day04/index.php
@@ -8,4 +8,17 @@ if ($age >=18) {
 
 ?>
 
+<?php
+//if...else Statement:
+$age = 16;
+
+if ($age >= 18) {
+    echo "You are eligible to vote.";
+} else {
+    echo "You are not eligible to vote.";
+}
+
+?>
+
+
 
