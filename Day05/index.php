8a1ac72a005f0b1b5ec2e93510dd1a47c1a0e41e
muaz64 <muazmuhammad21@gmail.com>
2025-03-21 18:03:38 +0600

    d05 php update1

---
+++ Day05/index.php
@@ -0,0 +1,18 @@
+<?php
+// for Loop
+
+for ($i = 1; $i <= 5; $i++) {
+    echo "Student $i has been graded.<br>";
+}
+?>
+
+<?php
+// while Loop
+$grade = 1;
+
+while ($grade <= 5) {
+    echo "Grading student $grade.<br>";
+    $grade++;
+}
+?>
+
