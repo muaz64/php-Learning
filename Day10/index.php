9b59d2a03b7d7fa10a39fa2d817de2b5eac7053d
muaz64 <muazmuhammad21@gmail.com>
2025-04-09 20:00:27 +0600

    d10 php update 4

--- Day10/index.php
+++ Day10/index.php
@@ -7,6 +7,7 @@ sort($books);       // Sorts the array in ascending order
 
 ?>
 <?php
+//2. Associative Arrays:
 $bookDetails = [
     "title" => "Harry Potter",
     "author" => "J.K. Rowling",
@@ -19,5 +20,18 @@ echo "<br>";
 foreach ($bookDetails as $key => $value) {
     echo "$key: $value <br>";
 }
+
+?>
+
+<?php
+//Multidimensional Arrays (Arrays within Arrays):
+$library = [
+    "Fiction" => ["Harry Potter", "The Hobbit"],
+    "Non-Fiction" => ["Sapiens", "Educated"],
+    "Science" => ["A Brief History of Time", "The Selfish Gene"]
+];
+echo $library["Fiction"][0] ; // Output: Harry Potter
+echo "<br>";
+echo $library["Science"][1] ; // Output: The Selfish Gene
 
 ?>
