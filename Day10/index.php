cc1ff6038262ffe6bf5d79982c8173cf6d78cde7
muaz64 <muazmuhammad21@gmail.com>
2025-04-09 19:00:55 +0600

    d10 php update1

--- Day10/index.php
+++ Day10/index.php
@@ -2,5 +2,7 @@
 //1.Indexed Arrays:
 $books = ["Harry Potter", "Lord of the Rings", "Percy Jackson"];
 echo $books[0];  // Output: Harry Potter
+echo count($books); // Counts the number of elements in the array
+sort($books);       // Sorts the array in ascending order
 
 ?>
