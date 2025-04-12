3880fe7f4943ef1c04ed88e4767507b49120977c
muaz64 <muazmuhammad21@gmail.com>
2025-04-12 12:15:32 +0600

    d12 php02

--- Day12/index.php
+++ Day12/index.php
@@ -4,4 +4,9 @@
   fwrite($file, "Hello, world!\n");
   fwrite($file, "Welcome to PHP file handling!");
   fclose($file);
+
+//   2. Appending to a File
+$file = fopen("example.txt", "a"); // "a" = append
+fwrite($file, "\nThis line is added later!");
+fclose($file);
 ?>
